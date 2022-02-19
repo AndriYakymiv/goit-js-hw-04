fe4af37954e7e6fe9d50434f1a31b167ff54baca
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-02-19 20:45:13 +0200

    Supplemented the makeMessage function so that it waits for the second parameter (callback parameter) of the callback function and returns its call.

---
+++ js/deliverPizzaMessage.js
@@ -0,0 +1,12 @@
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+function makeMessage(pizzaName, callback) {
+  const message = (callback(pizzaName))
+  return message;
+}
