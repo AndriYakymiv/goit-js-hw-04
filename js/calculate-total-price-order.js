94b08ba6dc7e95c890428943a1d684cc80b74168
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-02-21 19:17:26 +0200

    The calculateTotalPrice function takes one orderedItems.

---
+++ js/calculate-total-price-order.js
@@ -0,0 +1,12 @@
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+  
+orderedItems.forEach(item => {
+      totalPrice += item}
+  );
+
+  // Change code above this line
+  return totalPrice;
+}
