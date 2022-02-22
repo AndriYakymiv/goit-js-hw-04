9fa6ba05481e8f173aa11f271ad37586e363e927
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-02-22 20:21:13 +0200

    Refactor the calculateTotalPrice () function so that it uses implicit returns.

--- js/calculate-total-price-arrow.js
+++ js/calculate-total-price-arrow.js
@@ -2,3 +2,6 @@
 const calculateTotalPrice = (quantity, pricePerItem) => {
   return quantity * pricePerItem;
 }
+
+
+const calculateTotalPrice = (quantity, pricePerItem) => (quantity * pricePerItem)
