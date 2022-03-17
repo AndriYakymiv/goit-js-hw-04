82c417d870624a71d0b42c23babbbc9c9a1ca802
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-03-17 11:43:55 +0200

    Create get-active-users.js

---
+++ js/get-active-users.js
@@ -0,0 +1,6 @@
+// Change code below this line
+const getActiveUsers = (users) => {
+   const activeUser = users.filter(user => user.isActive)
+   return activeUser;
+};
+// Change code above this line
