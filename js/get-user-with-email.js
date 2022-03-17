f3b843d57f07fe27105fe6fd200a01048c37ef03
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-03-17 11:51:02 +0200

    Create get-user-with-email.js

---
+++ js/get-user-with-email.js
@@ -0,0 +1,6 @@
+// Change code below this line
+const getUserWithEmail = (users, email) => {
+   const userEmail = users.find(user => user.email === email);
+   return userEmail;
+};
+// Change code above this line
