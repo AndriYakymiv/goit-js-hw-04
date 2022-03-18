c0d8343cd6854dbabf036e456733e64782058e33
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-03-18 18:50:23 +0200

    Create is-every-user-active.

---
+++ js/is-every-user-active.js
@@ -0,0 +1,6 @@
+// Change code below this line
+const isEveryUserActive = (users) => {
+   const activeUser = users.every(user => user.isActive);
+   return activeUser;
+};
+// Change code above this line
