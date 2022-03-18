12bc29279c898cbff2d4405ea9d18b63e871e9a1
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-03-18 18:53:44 +0200

    Create is-any-user-active.js

---
+++ js/is-any-user-active.js
@@ -0,0 +1,6 @@
+// Change code below this line
+const isAnyUserActive = users => {
+   const activeUser = users.some(user => user.isActive)
+   return activeUser;
+};
+// Change code above this line
