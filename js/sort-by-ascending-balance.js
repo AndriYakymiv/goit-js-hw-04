5607eaf7b34c978eb89479acd584a05ac222dc45
Andrii Yakymiv <a.iakumiv@gmail.com>
2022-03-20 19:22:39 +0200

    Create sort-by-ascending-balance.js

---
+++ js/sort-by-ascending-balance.js
@@ -0,0 +1,5 @@
+// Change code below this line
+const sortByAscendingBalance = users => {
+   return users.sort((firstUser, secondUser) => firstUser.balance - secondUser.balance)
+};
+// Change code above this line
